724498db805ac1d05e5bf928966ff29a0cbd3df1
AnatoliiMishchenko <92863749+AnatoliiMishchenko@users.noreply.github.com>
2024-02-21 00:06:21 +0700

    Add files via upload

---
+++ Semm_6/src/main/java/org/example/MyButton.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MyButton extends JButton {
+    public int getIndex() {
+        return index;
+    }
+
+    private int index;
+
+    public MyButton(String text, int index) {
+        super(text);
+        this.index = index;
+    }
+
+    public MyButton(int index) {
+        super();
+        this.index = index;
+    }
+
+
+}
